a1efdd258c0c89a2d4596a35b7c6f0bf58be15ba
Onda-dev <manuelaltieri3@gmail.com>
2022-05-02 16:14:06 +0200

    Prova

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 // Scrivi un programma che stampi in console i numeri da 1 a 100.
-// const list = document.querySelector("ul.list");
 
 for( let i = 1; i <= 100; i++ ) {
     if( i % 3 === 0 && i % 5 === 0)
@@ -10,7 +9,14 @@ for( let i = 1; i <= 100; i++ ) {
     console.log("Buzz")
     else
     console.log(i)
+
+    const list = document.querySelector("ul.list");
+    const element = document.createElement("li");
+    element.innerText = i
+    list.append(element);
 }
+
+
 
 
 
