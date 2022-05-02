9abc041d814b9372f51aff8e75e7e5c84e80cf97
Onda-dev <manuelaltieri3@gmail.com>
2022-05-02 16:33:08 +0200

    Prova 2

--- js/main.js
+++ js/main.js
@@ -1,18 +1,23 @@
 // Scrivi un programma che stampi in console i numeri da 1 a 100.
+const element = document.createElement("li");
 
 for( let i = 1; i <= 100; i++ ) {
     if( i % 3 === 0 && i % 5 === 0)
+    element.innerText = "FizzBuzz"
     console.log("FizzBuzz")
     if( i % 3 === 0)
+    element.innerText = "Fizz"
     console.log("Fizz");
     if( i % 5 === 0)
+    element.innerText = "Buzz"
     console.log("Buzz")
     else
+    element.innerText = i
     console.log(i)
 
     const list = document.querySelector("ul.list");
-    const element = document.createElement("li");
-    element.innerText = i
+
+    // element.innerText = "fizz"
     list.append(element);
 }
 
