a24e1fe2402e4c9f9ed3fa26d16f0e0cd1561b04
Onda-dev <manuelaltieri3@gmail.com>
2022-05-02 18:17:18 +0200

    Milestone 3 ok

--- js/main.js
+++ js/main.js
@@ -4,14 +4,17 @@ for( let i = 1; i <= 100; i++ ) {
     const element = document.createElement("li");
     if( i % 3 === 0 && i % 5 === 0) {
         element.innerText = "FizzBuzz";
+        element.classList.add("green")
         console.log("FizzBuzz");
     }
     else if( i % 3 === 0) {
         element.innerText = "Fizz";
+        element.classList.add("red")
         console.log("Fizz");
     }
     else if( i % 5 === 0) {
         element.innerText = "Buzz";
+        element.classList.add("yellow")
         console.log("Buzz");
     }
     else {
