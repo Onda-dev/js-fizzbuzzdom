5b91f564d2415db24b2226f4168b3e6408724066
Onda-dev <manuelaltieri3@gmail.com>
2022-05-02 15:35:29 +0200

    Milestone 1 corretta

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
 // Scrivi un programma che stampi in console i numeri da 1 a 100.
-const list = document.querySelector("ul.list");
+// const list = document.querySelector("ul.list");
 
 for( let i = 1; i <= 100; i++ ) {
-    const element = document.createElement("li"); 
-    element.innerHTML = i;  
-    console.log(element)
+    if( i % 3 === 0 && i % 5 === 0)
+    console.log("FizzBuzz")
+    if( i % 3 === 0)
+    console.log("Fizz");
+    if( i % 5 === 0)
+    console.log("Buzz")
+    else
+    console.log(i)
 }
+
+
+
+
+
 
