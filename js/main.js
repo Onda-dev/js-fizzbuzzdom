40f22184e5ea022ea84c7a5b851f6382081acebb
Onda-dev <manuelaltieri3@gmail.com>
2022-05-02 16:43:09 +0200

    Buzz ok

--- js/main.js
+++ js/main.js
@@ -1,19 +1,24 @@
 // Scrivi un programma che stampi in console i numeri da 1 a 100.
-const element = document.createElement("li");
 
 for( let i = 1; i <= 100; i++ ) {
-    if( i % 3 === 0 && i % 5 === 0)
+    const element = document.createElement("li");
+    if( i % 3 === 0 && i % 5 === 0) {
     element.innerText = "FizzBuzz"
     console.log("FizzBuzz")
-    if( i % 3 === 0)
+    }
+    if( i % 3 === 0) {
     element.innerText = "Fizz"
     console.log("Fizz");
-    if( i % 5 === 0)
+    }
+    if( i % 5 === 0) {
     element.innerText = "Buzz"
     console.log("Buzz")
-    else
+    }
+    else {
     element.innerText = i
     console.log(i)
+    }
+
 
     const list = document.querySelector("ul.list");
 
